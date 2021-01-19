2635cab5c730f3f9ad74db43bd23747ab4a641e7
hellowuxin <hello_wuxin@qq.com>
2021-01-19 22:13:05 +0800

    deploy

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -8,6 +8,7 @@ const fs = require('fs')
     await execa('npm', ['run', 'build'])
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build'
+    await execa('touch', [`./${folderName}/.nojekyll`])
     await execa('git', ['--work-tree', folderName, 'add', '--all'])
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
     console.log('Pushing to gh-pages...')
