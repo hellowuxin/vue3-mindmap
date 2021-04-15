9a9b8537c1c016f62655d606633fec27b11ae0b9
hellowuxin <hello_wuxin@qq.com>
2021-04-15 15:07:28 +0800

    css module name

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import stringHash from 'string-hash'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,15 @@ export default defineConfig({
   base: '/vue3-mindmap/',
   build: {
     outDir: 'docs'
+  },
+  css: {
+    modules: {
+      generateScopedName: (name, filename, css) => {
+        const f = filename.split('?')[0].split('.')[0]
+        const file = path.basename(f)
+        const hash = stringHash(css).toString(36).substr(0, 5);
+        return `${file}_${name}_${hash}`
+      }
+    }
   }
 })
