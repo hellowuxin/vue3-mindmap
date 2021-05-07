65bbc2b8107c088478aeb61321a08699bb22fb92
hellowuxin <hello_wuxin@qq.com>
2021-05-07 21:15:13 +0800

    修改lib配置

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import stringHash from 'string-hash'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,16 @@ export default defineConfig({
         globals: {
           vue: 'Vue'
         }
+      }
+    }
+  },
+  css: {
+    modules: {
+      generateScopedName: (name, filename, css) => {
+        const f = filename.split('?')[0].split('.')[0]
+        const file = path.basename(f)
+        const hash = stringHash(css).toString(36).substr(0, 5);
+        return `${file}_${name}_${hash}`
       }
     }
   }
