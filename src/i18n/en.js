27c09fb29bb216c1c5690c08e5ef97b47d29b421
Iain Wood <mayasky76@gmail.com>
2023-05-13 14:30:46 +0000

    Update 6 files

---
+++ src/i18n/en.js
@@ -0,0 +1,21 @@
+export default {
+  translation: {
+    contextmenu: {
+      collapse: "Collapse",
+      expand: "Expand",
+      delete: "delete",
+      "delete-one": "Delete a single node",
+      add: "Add child node",
+      "add-parent": "Add parent node",
+      "add-sibling": "add sibling node",
+      "add-sibling-before": "add sibling node before",
+      cut: "cut",
+      copy: "copy",
+      paste: "paste",
+      selectall: "select all",
+      zoomin: "zoom in",
+      zoomout: "zoom out",
+      zoomfit: "zoom fit",
+    },
+  },
+};
